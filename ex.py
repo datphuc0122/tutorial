1dbe9f04d4c8c00bc25276d064886c644232f117
datphuc0122 <datphuc0122@gmail.com>
2023-02-02 22:24:39 +0700

    Create new file

---
+++ ex.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+np_mat = np.array([[1, 2],
+                   [3, 4],
+                   [5, 6]])
+# np_mat * 2
+nw = np_mat + np.array([10, 10])
+# np_mat + np_mat
+print(nw)
+        
